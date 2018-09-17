8692b7a82394ba18962f5595ff57a6acd170d134
Graham Dyson <grahamdyson@hotmail.com>
2018-09-17 12:07:47 +0100

    in test added package.json keys to avoid warnings

--- commonjs-babel-plugin/test.sh
+++ commonjs-babel-plugin/test.sh
@@ -25,7 +25,7 @@ function testPackages {
 	rm -r $testDirectoryPath
 	mkdir $testDirectoryPath
 	cd $testDirectoryPath
-	echo {} > package.json
+	echo "{ \"description\": \"test for $2\", \"license\": \"UNLICENSED\", \"repository\": \"none\" }" > package.json
 	npm install $2
 	npm install array.prototype.flatmap@1.2.1
 	npm install ../../../create-repository/output/devsnicket-plugin-discovery-create-repository*.tgz
