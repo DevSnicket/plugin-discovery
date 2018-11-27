593f632eb33452a0423891dde345a34ce9f3a145
Graham Dyson <grahamdyson@hotmail.com>
2018-11-27 11:59:31 +0000

    from plug-in tests removed redundant JSDOC types

--- commonjs-babel-plugin/tests/types.js
+++ commonjs-babel-plugin/tests/types.js
@@ -13,21 +13,4 @@
  * @property {string} repositoryPath
  */
 
-/**
- * @typedef {Object} setupParameter
- * @property {string} parameter.directory
- * @property {string} parameter.repositoryJavascript
- */
-
-/**
- * @callback setup
- * @param {setupParameter} parameter
- */
-
-/**
- * @typedef {Object} setupAndTestCases
- * @property {setup} setupAndTestCases.setup
- * @property {testCase[]} setupAndTestCases.testCases
-*/
-
 module.exports = null;
